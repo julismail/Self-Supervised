abcc8b3d33d283a8cb4159e92aa938e8a1a69967
julismail <setiajuli@yahoo.com>
2023-07-10 16:02:23 +0900

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,250 @@
+#to load the saved model
+import os
+import torch
+import torch.nn as nn
+import torchvision
+import pytorch_lightning as pl
+import lightly
+import matplotlib.pyplot as plt
+from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import normalize
+from PIL import Image
+import numpy as np
+resnet18_new = torchvision.models.resnet18()
+
+num_workers = 8
+#batch_size = 256
+batch_size = 256
+seed = 1
+#max_epochs = 20
+max_epochs = 100
+input_size = 128
+num_ftrs = 32
+
+pl.seed_everything(seed)
+
+# to create exactly the same backbone in order to load the weights
+backbone_new = nn.Sequential(*list(resnet18_new.children())[:-1])
+
+ckpt = torch.load('modelsimclr256100co.pth')
+backbone_new.load_state_dict(ckpt['resnet18_parameters'])
+
+#testing model, based on Moco
+import copy
+from lightly.models.utils import deactivate_requires_grad
+from lightly.models.utils import update_momentum
+from lightly.models.utils import batch_shuffle
+from lightly.models.utils import batch_unshuffle
+from pytorch_lightning.callbacks import TQDMProgressBar
+path_to_train = '/home/ismail/datasets/debisplit/training'
+path_to_test = '/home/ismail/datasets/debisplit/test'
+
+# data Augmentations to train on cifar-10
+train_classifier_transforms = torchvision.transforms.Compose([
+    torchvision.transforms.Resize((input_size, input_size)),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(
+        mean=lightly.data.collate.imagenet_normalize['mean'],
+        std=lightly.data.collate.imagenet_normalize['std'],
+    )
+])
+
+# No additional augmentations for the test set
+test_transforms = torchvision.transforms.Compose([
+    torchvision.transforms.Resize((input_size, input_size)),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(
+        mean=lightly.data.collate.imagenet_normalize['mean'],
+        std=lightly.data.collate.imagenet_normalize['std'],
+    )
+])
+
+#use the moco augmentations for training moco
+dataset_train_moco = lightly.data.LightlyDataset(
+    input_dir=path_to_train
+)
+
+dataset_train_classifier = lightly.data.LightlyDataset(
+    input_dir=path_to_train,
+    transform=train_classifier_transforms
+)
+
+dataset_test = lightly.data.LightlyDataset(
+    input_dir=path_to_test,
+    transform=test_transforms
+)
+
+dataloader_train_classifier = torch.utils.data.DataLoader(
+    dataset_train_classifier,
+    batch_size=batch_size,
+    shuffle=True,
+    drop_last=True,
+    num_workers=num_workers
+)
+
+dataloader_test = torch.utils.data.DataLoader(
+    dataset_test,
+    batch_size=batch_size,
+    shuffle=False,
+    drop_last=False,
+    num_workers=num_workers
+)
+
+class Classifier(pl.LightningModule):
+    def __init__(self, backbone):
+        super().__init__()
+        # use the pretrained ResNet backbone
+        self.backbone = backbone
+
+        # freeze the backbone
+        deactivate_requires_grad(backbone)
+
+        # create a linear layer for our downstream classification model
+        self.fc = nn.Linear(512, 2)
+
+        self.criterion = nn.CrossEntropyLoss()
+
+    def forward(self, x):
+        y_hat = self.backbone(x).flatten(start_dim=1)
+        y_hat = self.fc(y_hat)
+        return y_hat
+
+    def training_step(self, batch, batch_idx):
+        x, y, _ = batch
+        y_hat = self.forward(x)
+        loss = self.criterion(y_hat, y)
+        self.log("train_loss_fc", loss)
+        return loss
+
+    def training_epoch_end(self, outputs):
+        self.custom_histogram_weights()
+
+    # We provide a helper method to log weights in tensorboard
+    # which is useful for debugging.
+    def custom_histogram_weights(self):
+        for name, params in self.named_parameters():
+            self.logger.experiment.add_histogram(
+                name, params, self.current_epoch
+            )
+
+    def validation_step(self, batch, batch_idx):
+        x, y, _ = batch
+        y_hat = self.forward(x)
+        y_hat = torch.nn.functional.softmax(y_hat, dim=1)
+
+        # calculate number of correct predictions
+        _, predicted = torch.max(y_hat, 1)
+        num = predicted.shape[0]
+        correct = (predicted == y).float().sum()
+        return num, correct
+
+    def validation_epoch_end(self, outputs):
+        # calculate and log top1 accuracy
+        if outputs:
+            total_num = 0
+            total_correct = 0
+            for num, correct in outputs:
+                total_num += num
+                total_correct += correct
+            acc = total_correct / total_num
+            self.log("val_acc", acc, on_epoch=True, prog_bar=True)
+
+    def configure_optimizers(self):
+        optim = torch.optim.SGD(self.fc.parameters(), lr=30.)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optim, max_epochs)
+        return [optim], [scheduler]
+      
+#testing
+model.eval()
+classifier = Classifier(model.backbone)
+trainer = pl.Trainer(max_epochs=max_epochs, accelerator='gpu', devices=1,
+                     callbacks=[TQDMProgressBar(refresh_rate=100)])
+trainer.fit(
+    classifier,
+    dataloader_train_classifier,
+    dataloader_test
+)
+
+def generate_embeddings(model, dataloader):
+    """Generates representations for all images in the dataloader with
+    the given model
+    """
+
+    embeddings = []
+    filenames = []
+    with torch.no_grad():
+        for img, label, fnames in dataloader:
+            img = img.to(model.device)
+            emb = model.backbone(img).flatten(start_dim=1)
+            embeddings.append(emb)
+            filenames.extend(fnames)
+
+    embeddings = torch.cat(embeddings, 0)
+    embeddings = normalize(embeddings)
+    return embeddings, filenames
+
+
+model.eval()
+embeddings, filenames = generate_embeddings(model, dataloader_test)
+
+def get_image_as_np_array(filename: str):
+    """Returns an image as an numpy array
+    """
+    img = Image.open(filename)
+    return np.asarray(img)
+
+
+def plot_knn_examples(embeddings, filenames, n_neighbors=3, num_examples=6):
+    """Plots multiple rows of random images with their nearest neighbors
+    """
+    # lets look at the nearest neighbors for some samples
+    # we use the sklearn library
+    nbrs = NearestNeighbors(n_neighbors=n_neighbors).fit(embeddings)
+    distances, indices = nbrs.kneighbors(embeddings)
+
+    # get 5 random samples
+    samples_idx = np.random.choice(len(indices), size=num_examples, replace=False)
+
+    # loop through our randomly picked samples
+    for idx in samples_idx:
+        fig = plt.figure()
+        # loop through their nearest neighbors
+        for plot_x_offset, neighbor_idx in enumerate(indices[idx]):
+            # add the subplot
+            ax = fig.add_subplot(1, len(indices[idx]), plot_x_offset + 1)
+            # get the correponding filename for the current index
+            fname = os.path.join(path_to_data, filenames[neighbor_idx])
+            # plot the image
+            plt.imshow(get_image_as_np_array(fname))
+            # set the title to the distance of the neighbor
+            ax.set_title(f'd={distances[idx][plot_x_offset]:.3f}')
+            # let's disable the axis
+            plt.axis('off')
+
+
+  #plot color
+plot_knn_examples(embeddings, filenames)
+
+# Set color jitter and gray scale probability to 0
+new_collate_fn = lightly.data.SimCLRCollateFunction(
+    input_size=input_size,
+    vf_prob=0.5,
+    rr_prob=0.5,
+    cj_prob=0.0,
+    random_gray_scale=0.0
+)
+
+# let's update our collate method and reuse our dataloader
+dataloader_train_simclr.collate_fn = new_collate_fn
+
+# then train a new model
+model = SimCLRModel()
+trainer = pl.Trainer(
+    max_epochs=max_epochs, accelerator='gpu', devices=1
+)
+trainer.fit(model, dataloader_train_simclr)
+
+# and generate again embeddings from the test set
+model.eval()
+embeddings, filenames = generate_embeddings(model, dataloader_test)
+
